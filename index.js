22d4f31c555da82af803dcf76fa3956eaad18f4b
alejandro <alejandro@webcanopystudio.com>
2025-04-10 12:47:34 -0400

    Final version with correct headers, styling, and data rendering

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
+const HUBSPOT_BASE = 'https://api.hubapi.com';
+const HEADERS = {
+  Authorization: `Bearer ${process.env.PRIVATE_APP_TOKEN}`,
+  'Content-Type': 'application/json'
+};
+const OBJECT_TYPE_ID = process.env.OBJECT_TYPE_ID;
+
+/* ---------- middleware ---------- */
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+/* ---------- ROUTES ---------- */
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// homepage: list records
+app.get('/', async (req, res) => {
+  try {
+    const url = `${HUBSPOT_BASE}/crm/v3/objects/${OBJECT_TYPE_ID}?properties=name&type&properties=notes&limit=100`;
+    const { data } = await axios.get(url, { headers: HEADERS });
 
-// * Code for Route 1 goes here
+    const rows = data.results.map(r => ({
+      name: r.properties.name,
+      type: r.properties.type,
+      notes: r.properties.notes
+    }));
+    res.render('homepage', { title: 'Custom Object Table', rows });
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+   res.status(500).send(`Error loading records: ${JSON.stringify(err.response?.data || err.message)}`);
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+// render form
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+  });
 });
-*/
 
+// create record
+app.post('/update-cobj', async (req, res) => {
+  const { name, type, notes } = req.body;
+  try {
+    const url = `${HUBSPOT_BASE}/crm/v3/objects/${OBJECT_TYPE_ID}`;
+    await axios.post(
+      url,
+      { properties: { name, type, notes } },
+      { headers: HEADERS }
+    );
+    res.redirect('/');
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    res.status(500).send('Error creating record.');
+  }
+});
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
